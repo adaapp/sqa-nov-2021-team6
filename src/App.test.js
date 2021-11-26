a847820f141ae8f2e0003e827a85e1f6ab7cbdfc
Steve Johnstone <t-sjohnstone@hotels.com>
2021-11-26 13:36:19 +0000

    Basic UI interface and component structure created

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('it render the main screen correctly', () => {
+  const { getByText, getByRole } = render(<App />);
+  
+  //Check for heading
+  expect(getByText('RGB to Hex Converter')).not.toBeNull();
+
+  //Check for buttons
+  expect(getByRole('button', { name:/Convert/i })).not.toBeNull();
+  expect(getByRole('button', { name:/Reset/i })).not.toBeNull();
+
+  //Check for color input fields
+  expect(getByText('red')).not.toBeNull();
+  expect(getByText('green')).not.toBeNull();
+  expect(getByText('blue')).not.toBeNull();
 });
