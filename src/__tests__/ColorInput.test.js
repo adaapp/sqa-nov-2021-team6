c1c8628c8149e01245fd39c9024588f6368aae39
YuliaPichugina <t-ypichugina@hotels.com>
2021-12-09 16:47:55 +0000

    fix failing test

--- src/__tests__/ColorInput.test.js
+++ src/__tests__/ColorInput.test.js
@@ -2,12 +2,12 @@ import { render, fireEvent } from "@testing-library/react";
 import ColorInput from "../components/ColorInput";
 
 test("getColor called when user input changes", () => {
-    const getColor = jest.fn()
-    const { getByLabelText } = render(<ColorInput color={"green"} getColor={getColor}/>);
+    const setColor = jest.fn()
+    const { getByLabelText } = render(<ColorInput color={"green"} setColor={setColor}/>);
 
     fireEvent.change(getByLabelText("green"), {
         target: { value: "255" }
       });
   
-    expect(getColor).toHaveBeenCalled();
+    expect(setColor).toHaveBeenCalled();
   });
