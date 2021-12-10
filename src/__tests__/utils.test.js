c224a141226e945eb4801308351298653e84922e
Steve Johnstone <t-sjohnstone@hotels.com>
2021-12-10 16:35:50 +0000

    additional tests for invalid inputs, plus eslint extended to test files

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,7 +1,29 @@
 import { singleColorConverter } from "../components/utils.js";
 
-test("singleColorConverter returns correct value", () => {
-    expect(singleColorConverter("23")).toBe("17");
-    expect(singleColorConverter("155")).toBe("9b");
-  });
+test("singleColorConverter returns correct value from a valid input", () => {
+  expect(singleColorConverter("1")).toBe("01");
+  expect(singleColorConverter("23")).toBe("17");
+  expect(singleColorConverter("99")).toBe("63");
+  expect(singleColorConverter("155")).toBe("9b");
+  expect(singleColorConverter("222")).toBe("de");
+  expect(singleColorConverter("255")).toBe("ff");
+});
+
+test("singleColorConverter throws an error when the input field is empty", () => {
+  expect(singleColorConverter("")).toThrow("Please enter a value beteween 0 and 255");
+});
+
+test("singleColorConverter throws an error when the input is not a string", () => {
+  expect(singleColorConverter(4)).toThrow("Input must be a string");
+  expect(singleColorConverter(true)).toThrow("Input must be a string");
+  expect(singleColorConverter(null)).toThrow("Input must be a string");
+  expect(singleColorConverter(undefined)).toThrow("Input must be a string");
+  expect(singleColorConverter({})).toThrow("Input must be a string");
+  expect(singleColorConverter([])).toThrow("Input must be a string");
+});
+
+test("singleColorConverter throws an error when the input is a string but the number is outside the expected range (0 - 255)", () => {
+  expect(singleColorConverter("-5")).toThrow("RGB values must be between 0 and 255");
+  expect(singleColorConverter("276")).toThrow("RGB values must be between 0 and 255");
+});
 
