b5a47f07c6b356c44cfd386d9cf6536f784fe537
Zach Molony <31472522+zachmolony@users.noreply.github.com>
2021-12-13 18:28:18 +0000

    feat: update pipeline to handle puppeteer

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -29,18 +29,21 @@ describe("App.js", () => {
     //Check for HEX color display
     expect(getByText("HEX Colour")).toBeInTheDocument();
   });
-})
+});
 
 describe("the user interface should work correctly", () => {
-
   const app = "http://localhost:3000/";
-  let browser; 
+  let browser;
   let page;
   let hexNumber;
 
   it("should correctly convert the inputs to a HEX number when the convert button is clicked", async () => {
     //Open browser and open app in new page
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      executablePath: process.env.PUPPETEER_EXEC_PATH,
+      headless: false
+    });
     page = await browser.newPage();
     await page.goto(app);
 
@@ -56,15 +59,22 @@ describe("the user interface should work correctly", () => {
     await page.click("button[name='Convert']");
 
     //Check the correct HEX value is displayed to the user
-    hexNumber = await page.$eval("input[name='display-color']", el => el.value);
+    hexNumber = await page.$eval(
+      "input[name='display-color']",
+      (el) => el.value
+    );
     expect(hexNumber).toBe("#64dc4b");
 
     await browser.close();
-  })
+  });
 
   it("should clear all values from the input fields as well as the HEX color display, when the reset button is clicked", async () => {
     //Open browser and open app in new page
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      executablePath: process.env.PUPPETEER_EXEC_PATH,
+      headless: false
+    });
     page = await browser.newPage();
     await page.goto(app);
 
@@ -78,35 +88,54 @@ describe("the user interface should work correctly", () => {
 
     //Click on the convert button
     await page.click("button[name='Convert']");
-  
+
     //Check the correct HEX value is displayed to the user
-    hexNumber = await page.$eval("input[name='display-color']", el => el.value);
+    hexNumber = await page.$eval(
+      "input[name='display-color']",
+      (el) => el.value
+    );
     expect(hexNumber).toBe("#d22d8a");
 
     //Click on the reset button
     await page.click("button[name='Reset']");
 
     //Check that the red value is reset
-    const redValue = await page.$eval("input[name='Red']", el => el.textContent);
+    const redValue = await page.$eval(
+      "input[name='Red']",
+      (el) => el.textContent
+    );
     expect(redValue).toBe("");
 
     //Check that the green value is reset
-    const greenValue = await page.$eval("input[name='Green']", el => el.textContent);
+    const greenValue = await page.$eval(
+      "input[name='Green']",
+      (el) => el.textContent
+    );
     expect(greenValue).toBe("");
 
     //Check that the blue value is reset
-    const blueValue = await page.$eval("input[name='Blue']", el => el.textContent);
+    const blueValue = await page.$eval(
+      "input[name='Blue']",
+      (el) => el.textContent
+    );
     expect(blueValue).toBe("");
 
     //Check that the hex value is reset
-    hexNumber = await page.$eval("input[name='display-color']", el => el.value);
+    hexNumber = await page.$eval(
+      "input[name='display-color']",
+      (el) => el.value
+    );
     expect(hexNumber).toBe("#000000");
 
     await browser.close();
-  })
+  });
 
   it("should not convert to a HEX number if one of the color input fields is empty", async () => {
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      executablePath: process.env.PUPPETEER_EXEC_PATH,
+      headless: false
+    });
     page = await browser.newPage();
     await page.goto(app);
 
@@ -118,16 +147,22 @@ describe("the user interface should work correctly", () => {
     await page.click("button[name='Convert']");
 
     //Check that defauly HEX value is still displayed
-    hexNumber = await page.$eval("input[name='display-color']", el => el.value);
+    hexNumber = await page.$eval(
+      "input[name='display-color']",
+      (el) => el.value
+    );
     expect(hexNumber).toBe("#000000");
 
     await browser.close();
-  })
+  });
 
   it("should not convert to a HEX number if a number outside the expected range is entered into one of the color input fields", async () => {
-  
     //Open browser and open app in new page
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      executablePath: process.env.PUPPETEER_EXEC_PATH,
+      headless: false
+    });
     page = await browser.newPage();
     await page.goto(app);
 
@@ -143,9 +178,12 @@ describe("the user interface should work correctly", () => {
     await page.click("button[name='Convert']");
 
     //Check that defauly HEX value is still displayed
-    hexNumber = await page.$eval("input[name='display-color']", el => el.value);
+    hexNumber = await page.$eval(
+      "input[name='display-color']",
+      (el) => el.value
+    );
     expect(hexNumber).toBe("#000000");
 
     await browser.close();
-  })
-})
+  });
+});
