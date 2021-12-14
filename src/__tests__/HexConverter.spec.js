4cb02e8a3aec20e58e81656d34860dfd219d438b
Zach Molony <31472522+zachmolony@users.noreply.github.com>
2021-12-14 20:10:26 +0000

    task: add hexConverter unit test

--- src/__tests__/HexConverter.spec.js
+++ src/__tests__/HexConverter.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import HexConverter from "../components/HexConverter";
 
 describe("HexConverter", () => {
@@ -19,8 +19,64 @@ describe("HexConverter", () => {
   });
 
   describe("logic", () => {
-    it.todo("should set hex colour based on input");
+    it.each([
+      ["100", "200", "250", "#64c8fa"],
+      ["1", "2", "3", "#010203"],
+      ["0", "100", "255", "#0064ff"]
+    ])(
+      "should set hex colour based on input",
+      async (red, green, blue, hexValue) => {
+        const { getByLabelText, getByDisplayValue, getByText } = render(
+          <HexConverter />
+        );
 
-    it.todo("should reset state");
+        await fireEvent.change(getByLabelText("Red"), {
+          target: { value: red }
+        });
+
+        await fireEvent.change(getByLabelText("Green"), {
+          target: { value: green }
+        });
+
+        await fireEvent.change(getByLabelText("Blue"), {
+          target: { value: blue }
+        });
+
+        fireEvent.click(getByText("Convert", { selector: "button" }));
+
+        expect(getByDisplayValue(red)).toBeInTheDocument();
+        expect(getByDisplayValue(green)).toBeInTheDocument();
+        expect(getByDisplayValue(blue)).toBeInTheDocument();
+        expect(getByDisplayValue(hexValue)).toBeInTheDocument();
+      }
+    );
+
+    it.each([
+      ["100", "200", "250", "#000000"],
+      ["1", "2", "3", "#000000"],
+      ["0", "100", "255", "#000000"]
+    ])("should reset state", async (red, green, blue, hexValue) => {
+      const { getByLabelText, getByTestId, queryAllByDisplayValue, getByText } =
+        render(<HexConverter />);
+
+      await fireEvent.change(getByLabelText("Red"), {
+        target: { value: red }
+      });
+
+      await fireEvent.change(getByLabelText("Green"), {
+        target: { value: green }
+      });
+
+      await fireEvent.change(getByLabelText("Blue"), {
+        target: { value: blue }
+      });
+
+      fireEvent.click(getByText("Reset", { selector: "button" }));
+
+      expect(queryAllByDisplayValue(red).length).toBe(0);
+      expect(queryAllByDisplayValue(green).length).toBe(0);
+      expect(queryAllByDisplayValue(blue).length).toBe(0);
+      expect(getByTestId("display-hex").value).toBe(hexValue);
+    });
   });
 });
--- src/components/ColorInput.js
+++ src/components/ColorInput.js
@@ -3,13 +3,15 @@ import ReactColorSquare from "react-color-square";
 import PropTypes from "prop-types";
 
 const ColorInput = ({ color, setColor }) => {
-
   const [errorMessage, setErrorMessage] = useState("");
 
   const verifyInput = (userInputValue) => {
-
-    const parsedInputValue = (parseInt(userInputValue, 10));
-    if (parsedInputValue < 0 || parsedInputValue > 255 || isNaN(parsedInputValue)) {
+    const parsedInputValue = parseInt(userInputValue, 10);
+    if (
+      parsedInputValue < 0 ||
+      parsedInputValue > 255 ||
+      isNaN(parsedInputValue)
+    ) {
       setErrorMessage("Please insert a number between 0 and 255");
       setColor(userInputValue);
     } else {
@@ -22,13 +24,17 @@ const ColorInput = ({ color, setColor }) => {
     <div>
       <form className="field py-4">
         <div className="control">
-          <label style={{color: color}} htmlFor="text" className="label">
+          <label
+            style={{ color: color }}
+            htmlFor={"text" + color}
+            className="label"
+          >
             {color}
           </label>
           <div className="level">
             <input
               name={color}
-              id="text"
+              id={"text" + color}
               className="input level-item"
               placeholder="e.g. 123"
               type="text"
@@ -40,7 +46,7 @@ const ColorInput = ({ color, setColor }) => {
           </div>
         </div>
       </form>
-      <div >
+      <div>
         <p className="has-text-danger">{errorMessage}</p>
       </div>
     </div>
@@ -49,7 +55,7 @@ const ColorInput = ({ color, setColor }) => {
 
 ColorInput.propTypes = {
   color: PropTypes.string,
-  setColor: PropTypes.func,
+  setColor: PropTypes.func
 };
 
 export default ColorInput;
